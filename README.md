937ab19b5dd552b6aa2318f4738626945b167923
Andrea Little <andrealittle999@yahoo.com>
2021-02-26 12:01:56 -0800

    All images

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Coronavirus data in Washington
+# Coronavirus data in Washington
 
 # The Data
 
@@ -25,4 +25,39 @@ The second dataframe is sector_10k, which is the combined dataframe from the two
 
 The columns of this data set are ['Cases_Hispanic', 'Cases_White', 'Cases_Asian', 'Cases_Black',
        'Cases_AIAN', 'Cases_Total']
+The indexes are ['Health Care', 'Manufacturing', 'Retail', 'Agriculture', 'Food Service',
+       'Construction', 'Transportation', 'Public Admin']
+
+The following three graphs show the trend of cases, deaths, and hospitalizations from April 2020 to the present. 
+There is a rise in all three categories in this time period, with a big spike around November of 2020. 
+
+{fig 1, 2, 3}
+
+The above graphs show a raw count of cases. However, this is not an accurate representation of which races were most affected, as there are more White people than any other race. Note that even though there are much more White people than Hispanic people, there are still more Hispanic cases. The following graphs are adjusted per 10,000. For number of cases, hospitalizations, and deaths, Hispanics are harder hit than any other race by a huge margin. Black people fare slightly worse than Whites, Asians, and Native Americans, but no where near Hispanics. Why is this the case? It may be that more Hispanics are in industries where you are more likely to contract coronavirus. 
+
+fig 4, 5, 6
+
+# Analysis 
+
+Is there a significant difference in cases between each industry? The 8 industries are health care, manufacturing, retail, agriculture, food service, construction, transportation and public admininistration. With p < .05, 64 null hypothesis significance tests were carried out. 
+As an example, is there a difference in cases between the retail and manufacturing industry? In the dataframe, there are about 202 cases per 10,000 in the manufacturing industry, and about 188 cases per 10,000 in the retail industry. 
+
+fig 7 
+
+Null hypothesis - There is no difference in corona cases in the retail industry vs the manufacturing industry
+Alternative hypothesis - There are more cases in the manufacturing industry than the retail industry
+This figure is the distribution of (Cases in retail) - (Cases in manufacturing). Probability of seeing this observed difference or higher given the null hypothesis is 0.285. With a p-value of 0.285, we cannot reject the null hypothesis. 
+The following heat map gives the p values between all the industries. 
+
+fig 8 
+
+In this figure, the diagonal is meaningless, because it is each industry versus itself. The results are repeated across the axis (health care, manufacturing is the same as manufacturing, health care) so looking at one half of the diagonal will give all the information you need. There is a significant difference in cases between the health industry and every other industry. There is a difference between manufacturing and agriculture, construction, transportation, and public admin. Looking at the whole chart, there is a difference between a lot of the industries. 
+
+# Conclusion
+
+The rollout of vaccines in Washington is by tier. Tier 1 and 2 included health care workers. This is in line with the results of the analysis, as the health care industry had more cases than any other industry. However, no other Tier gives priority to people in certain industries, even though some industries other than health care were riskier environments for contracting covid. Also, priority is not given to Hispanics, and they contract covid at more than 3x the rate of whites. In tier 3, vaccines are given to those with 2 or more comorbidities. Comorbidities include obesity and smoking. This doesn't seem right, as someone's race is something that absolutely cannot be helped. Someone's industry is mostly something they cannot help. Some would argue that one could change industries if they were worried about contracting covid, but realistically most do not have this option. Whether someone is obese or smokes is something that people have more control over than their race or occupation. If a ranking of who is most at risk to get covid was used for vaccine distribution insteead of tiers, the results would be more fair and there would be less cases overall. A variety of factors could be used to analyze each persons risk, such as race, occupation, and comorbidities. 
+
+
+
+
 
--- read_data.py
+++ read_data.py
@@ -7,9 +7,11 @@ import seaborn as sns; sns.set_theme()
 
 df = pd.read_csv("washington-race.csv", delimiter = ',')
 
+# DATA CLEANING for dataframe "df"
 # drop empty columns
 # LatinX - ancetry from Latin American countries regardless of language, Hispanic - ancestry from Spanish-speaking countries
-    # Combine to Hispanic - includes both these groups in this df
+# Combine to Hispanic - includes both these groups in this df
+# fill Na with mean of column, change date to DateTime
 df.drop(['Tests_AIAN', 'Tests_Asian', 'Tests_Black', 'Tests_Ethnicity_Hispanic', 'Tests_Ethnicity_NonHispanic', 'Tests_Ethnicity_Unknown',
     'Tests_LatinX', 'Tests_Multiracial', 'Tests_NHPI', 'Tests_Other', 'Tests_White', 'Tests_Total', 'State'], axis = 1, inplace = True, )
 df['Cases_Hispanic'] = df['Cases_Ethnicity_Hispanic'] + df['Cases_LatinX']
@@ -19,7 +21,6 @@ cols = ['Cases_Asian', 'Cases_AIAN', 'Cases_Black', 'Cases_White', 'Cases_Hispan
     'Deaths_Asian', 'Deaths_AIAN', 'Deaths_Black', 'Deaths_White', 'Deaths_Hispanic', 'Deaths_Total', 
     'Hospitalizations_Asian', 'Hospitalizations_AIAN', 'Hospitalizations_Black', 'Hospitalizations_White', 
     'Hospitalizations_Hispanic', 'Hospitalizations_Total']
-# fill Na with mean of column, change date to DateTime
 nan_values = df.isna()
 nan_columns = nan_values.any()
 columns_with_nan = df.columns[nan_columns].tolist()
@@ -27,20 +28,20 @@ for column in columns_with_nan:
     df[column].fillna(value=df[column].mean(), inplace=True)
 df['Date'] = pd.to_datetime(df['Date'], format='%Y%m%d', errors='coerce')
 
-#df['Hospitalizations_White'].fillna(value=df['Hospitalizations_White'].mean(), inplace=True)
-#print(df['Hospitalizations_White'])
-
-
-
-def plot_by_date(columns, labels, title, df):
+# function to plot coronavirus cases, deaths, or hospitalizations through time
+def plot_by_date(columns, labels, title, df, xlabel, ylabel):
     fig, ax = plt.subplots()
+    fig.tight_layout(pad = 4)
     for tup in zip(columns, labels):
         ax.plot(df['Date'], df[tup[0]], label = tup[1])
     ax.legend()
-    ax.set_title(title)
-    plt.xticks(rotation = 90)
-    ax.tick_params(axis='x', which='major', labelsize=5)
-    plt.show()  
+    ax.set_title(title, size = 14.5)
+    ax.set_xlabel(xlabel, size = 9)
+    ax.set_ylabel(ylabel, size = 9)
+    plt.xticks(rotation = 90, size = 9)
+    ax.tick_params(axis='x', which='major', labelsize=8)
+    ax.tick_params(axis='y', which='major', labelsize=8)
+    plt.savefig()
 
 Races_cases = ['Cases_Asian', 'Cases_AIAN', 'Cases_Black', 'Cases_White', 'Cases_Hispanic', 'Cases_Total']
 Races_deaths = ['Deaths_Asian', 'Deaths_AIAN', 'Deaths_Black', 'Deaths_White', 'Deaths_Hispanic', 'Deaths_Total']
@@ -48,22 +49,21 @@ Races_hospitalization = ['Hospitalizations_Asian', 'Hospitalizations_AIAN', 'Hos
     'Hospitalizations_Hispanic', 'Hospitalizations_Total']
 Labels = ["Asian", "Native American", "Black", "White", "Hispanic", "Total"]
 
-#plot_by_date(Races_cases, Labels, "Corona cases for all the races", df)
-#plot_by_date(Races_deaths, Labels, "Corona deaths for all the races", df)
-#plot_by_date(Races_hospitalization, Labels, "Corona hospitalizations for all the races", df)
+# Plots raw counts
+plot_by_date(Races_cases, Labels, "Corona cases for all the races", df, 'Date', 'Number Cases')
+plot_by_date(Races_deaths, Labels, "Corona deaths for all the races", df, 'Date', 'Number Cases')
+plot_by_date(Races_hospitalization, Labels, "Corona hospitalizations for all the races", df, 'Date', 'Number Cases')
 
-#print(df.columns)
-
-# df_prop has only the cases. This dataframe sums all the cases from all the days, and they are converted to cases per 10,000 people 
-# This is adjusted from the fact that Washington is 9.6% Asian, 1.9% Native American, 4.4% Black, 67.5% White, 13% Hispanic
-    # with a population of 7,614,893
+# DATA CLEANING 
+# df_prop has only the cases. This dataframe sums all the cases from all the days, and they are converted to 
+# cases per 10,000 people 
+# This is adjusted from the fact that Washington is 9.6% Asian, 1.9% Native American, 4.4% Black, 67.5% White, 
+# 13% Hispanic with a population of 7,614,893
 # df_prop will only have the first row of df, which is the total number of cases on 02/21/2021 (NOT new cases) as this is the most recent data
 num_eachrace = np.array([.096, .019, .044, .675, .13, 1, .096, .019, .044, .675, .13, 1, .096, .019, .044, .675, .13, 1])
 num_eachrace *= 7614893
 num_eachrace *= (1 / 10000)
-
 num_eachrace = pd.DataFrame([num_eachrace], columns = cols)
-
 # row 3 is cases per 10,000
 df_prop = df[cols]
 # df_prop = df_prop.loc[0:1, :]
@@ -72,11 +72,11 @@ for i in range(91):
     df_prop.loc[i] = df_prop.loc[i] / df_prop.loc[91]
 df_prop['Date'] = df['Date']
 df_prop.drop([91], axis = 0, inplace=True)
-#print(df_prop)
-#plot_by_date(Races_cases, Labels, "Corona cases per 10,000 for all the races", df_prop)
-#plot_by_date(Races_deaths, Labels, "Corona deaths per 10,000 for all the races", df_prop)
-# plot_by_date(Races_hospitalization, Labels, "Corona hospitalizations per 10,000 for all the races", df_prop)
 
+# Plots per 10,000
+plot_by_date(Races_cases[0:5], Labels[0:5], "Corona cases per 10,000 for all the races", df_prop, 'Date', 'Number Cases per 10,000')
+plot_by_date(Races_deaths[0:5], Labels[0:5], "Corona deaths per 10,000 for all the races", df_prop, 'Date', 'Number Cases per 10,000')
+plot_by_date(Races_hospitalization[0:5], Labels[0:5], "Corona hospitalizations per 10,000 for all the races", df_prop, 'Date', 'Number Cases per 10,000')
 
 #df_prop_sector is the first row of df, to be used to merge with the sector data
 df_prop_sector = df_prop.loc[0:1, :]
@@ -85,40 +85,27 @@ df_prop_sector.drop(['Deaths_Asian', 'Deaths_AIAN',
        'Hospitalizations_Asian', 'Hospitalizations_AIAN',
        'Hospitalizations_Black', 'Hospitalizations_White',
        'Hospitalizations_Hispanic', 'Hospitalizations_Total', 'Date', 'Cases_Total'], axis = 1, inplace=True)
-#df_prop_sector['Sector'] = ['Filler', 'Filler']
 
-
+# DATA CLEANING, and combining population-percent.csv with df from above
 df_sector = pd.read_csv("population-percent.csv", delimiter = ',,', engine = 'python')
 
-
-# print(df_sector.head())
-# print(df_prop_sector)
 
 df_sector = df_sector.rename(columns={'Race / Ethnicity': 'Sector', 'Hispanic': 'Cases_Hispanic', 'Non-Hispanic White':'Cases_White', 
     'Non-Hispanic Asian':'Cases_Asian', 'Non-Hispanic Black':'Cases_Black', ' American Indian or Alaska Native': 'Cases_AIAN'})
 df_sector.drop(0, axis = 0, inplace = True)
-
 df_sector.drop("Unnamed: 6", axis = 1, inplace = True)
 df_sector.reset_index(inplace=True)
 df_sector.drop("index", axis = 1, inplace = True)
-
-
 for cases in Races_cases[0:5]:
     df_sector[cases] = df_sector[cases].str.rstrip('%').astype('float') / 100.0
 
-
-# print(df_sector.head())
 # Now we have all the percents as decimals from rows 0:7, and from 8:9 the number of cummulative cases on 02/21/2021
-# Next step: multiply rows -0:7 by row 8, to get cases per 10,000 in each industry sector
+# Next step: multiply rows 0:7 by row 8, to get cases per 10,000 in each industry sector
 sector_final = pd.concat([df_sector, df_prop_sector], ignore_index=True)
-
-#print(sector_final[['Cases_Asian', 'Cases_AIAN']])
 Races_cases = ['Cases_Asian', 'Cases_AIAN', 'Cases_Black', 'Cases_White', 'Cases_Hispanic', 'Cases_Total']
-
 temp = sector_final.drop(['Sector'], axis = 1)
 for i in range(8):
     temp.loc[i] = temp.loc[i] * temp.loc[8]
-#print(temp)
 #sector_10k has the cases per industry per 10,000
 sector_10k = sector_final[['Sector']].join(temp)
 #print(sector_10k[['Sector', 'Cases_Hispanic']])
@@ -132,32 +119,11 @@ sector_10k = sector_10k.rename(index = {'"Agriculture, Forestry, Fishing and Hun
 
 #Hypothesis testing
 # THe health care industry is seen generally as the most dangerous, and thus health care workers were one of the first to 
-# receive the vaccine. However, retail and food services also appear to be dangerous trades. Is there a 
-# significant difference between the safest job(public administration) and retail, and food services?
-# Starting with Hispanics in retail vs Hispanics in public administration 
-
-#Retail - row 2, Public admin - row 7
-# print(sector_10k['Cases_Hispanic'])
+# receive the vaccine. However, other sectors also appear to have a high ammount of cases. Test for a significant 
+# difference between all industries
+print(sector_10k)
 sector_10k['Cases_Total'] = sector_10k.sum(axis = 1)
-print(sector_10k['Cases_Total'])
-print(sector_10k.columns)
-
-
-#Null hypothesis- there is no difference between covid cases in retail and covid cases in public admin
-# total_Hisp = sector_10k["Cases_Total"].sum(axis = 0)
-# shared_freq = (sector_10k.loc[2, "Cases_Total"] + sector_10k.loc[7, "Cases_Total"]) / total_Hisp
-# # print(shared_freq)
-# shared_var = (2 * (shared_freq) * (1 - shared_freq))/total_Hisp
-# # print(shared_var)
-# diff_prop = stats.norm(0, np.sqrt(shared_var))
-# diff_in_sample_prop = (sector_10k.loc[2, "Cases_Total"] - sector_10k.loc[7, "Cases_Total"])  / total_Hisp
-# p_val = 1 - diff_prop.cdf(diff_in_sample_prop)
-# # print(shared_freq)
-# # print(total_Hisp)
-# print(p_val)
-
-
-
+# Find the p-value for whether there is a significant difference in covid cases between two industries 
 def find_p(industry1, industry2):
     all_industries_cases = sector_10k["Cases_Total"].sum(axis = 0)
     shared_freq = (sector_10k.loc[industry1, "Cases_Total"] + sector_10k.loc[industry2, "Cases_Total"]) / all_industries_cases
@@ -173,39 +139,39 @@ shared_freq = (sector_10k.loc['Retail', "Cases_Total"] + sector_10k.loc['Manufac
 shared_var = (sector_10k.loc['Retail', "Cases_Total"] + sector_10k.loc['Manufacturing', "Cases_Total"]) * (shared_freq) * (1 - shared_freq)
 diff_prop = stats.norm(0, np.sqrt(shared_var))
 diff_in_sample = abs(sector_10k.loc['Retail', "Cases_Total"] - sector_10k.loc['Manufacturing', "Cases_Total"]) 
-# fig, ax = plt.subplots()
-# x = np.linspace(-50, 50, num=250)
-# palette = sns.color_palette("mako_r", 6)
-# ax = sns.lineplot(
-#     x= x, y=diff_prop.pdf(x),
-#     palette=palette)
-# ax.fill_between(x, diff_prop.pdf(x), 
-#     where=(x >= diff_in_sample), color="red", alpha=0.5)
-# p = find_p('Retail', 'Manufacturing')
-# ax.set_title("Retail vs Manufacturing p-val region, p = 0.285")
-# ax.set_xlabel("Cases Manufacturing - Cases Retail", fontsize = 9)
-# plt.show()
+
+# plot of p-value region for Manufacturing vs Retail 
+fig, ax = plt.subplots()
+x = np.linspace(-50, 50, num=250)
+palette = sns.color_palette("mako_r", 6)
+ax = sns.lineplot(
+    x= x, y=diff_prop.pdf(x),
+    palette=palette)
+ax.fill_between(x, diff_prop.pdf(x), 
+    where=(x >= diff_in_sample), color="red", alpha=0.5)
+p = find_p('Retail', 'Manufacturing')
+ax.set_title("Retail vs Manufacturing p-val region, p = 0.285")
+ax.set_xlabel("Cases Manufacturing - Cases Retail", fontsize = 9)
+plt.savefig()
 
 
-#print(find_p('Manufacturing', 'Food Service'))
-print(sector_10k.loc["Retail", "Cases_Total"])
+# Make the np arrray of p-values for all hypothesis tests for each industry vs each other industry
+#print(sector_10k.loc["Retail", "Cases_Total"])
 array_pval = np.empty((8, 8))
 for industry, i in zip(sector_10k.index, range(8)):
     for other_industry, j in zip(sector_10k.index, range(8)):
         array_pval[i, j] = find_p(industry, other_industry)
 
-print(df.columns)
-# print(array_pval[array_pval < .05])
-# fig, ax = plt.subplots()
-# fig.tight_layout(pad = 5)
-# sns.set(font_scale = 0.8)
-# ax = sns.heatmap(np.array(array_pval), xticklabels = sector_10k.index, yticklabels = sector_10k.index)
-# ax.set_xticklabels(ax.get_xmajorticklabels(), fontsize = 8.5, rotation = 90)
-# ax.set_yticklabels(ax.get_xmajorticklabels(), fontsize = 8.5)
-# ax.set_title("p-values across industries", fontsize = 13)
-# plt.show()
+# heatmap of p-values for all hypothesis tests 
+fig, ax = plt.subplots()
+fig.tight_layout(pad = 5)
+sns.set(font_scale = 0.8)
+ax = sns.heatmap(np.array(array_pval), xticklabels = sector_10k.index, yticklabels = sector_10k.index)
+ax.set_xticklabels(ax.get_xmajorticklabels(), fontsize = 8.5, rotation = 90)
+ax.set_yticklabels(ax.get_xmajorticklabels(), fontsize = 8.5)
+ax.set_title("p-values across industries", fontsize = 13)
+plt.savefig()
 
-#a = pd.DataFrame(array_pval)
 
 
 
